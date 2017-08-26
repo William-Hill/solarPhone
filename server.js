0fdfa7b0e0c76f1260c7a7be4dbb2b39b05d1f02
William Hill <mjh2225@gmail.com>
2017-08-26 15:18:28 -0700

    added routes for creating and getting customer

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8080;
+
+const customerController = require('/controllers/customer.controller.js')
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/dist/index.html');
 });
+
+app.post('/customer', function(request, response){
+  customerController.createCustomer(request, response);
+});
+
+app.get('/customer:customerID', function(request, response){
+  customerController.getCustomer(request, response);)
+})
 
 app.listen(PORT, error => {
   error
