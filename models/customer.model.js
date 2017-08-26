7369460b577e89c1616e8b6e9a11e3497c3b36d0
William Hill <mjh2225@gmail.com>
2017-08-26 14:52:24 -0700

    added customer model

---
+++ models/customer.model.js
@@ -0,0 +1,18 @@
+//Require Mongoose
+var mongoose = require('mongoose');
+
+//Define a schema
+var Schema = mongoose.Schema;
+
+var customerSchema = new Schema({
+    name: String,
+    address: String,
+    email: String
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var customer = mongoose.model('customer', customerSchema);
+
+// make this available to our users in our Node applications
+module.exports = customer;
